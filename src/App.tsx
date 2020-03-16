e1f2b241daf3c1b83f00e17375d0ce17d872738e
lebull <bulldogboy912000@yahoo.com>
2020-03-15 21:01:55 -0500

    Add basic fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       <Cheat/>
+
+      <div>
+        <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
+        <form>
+          <input type="hidden" defaultValue={process.env.REACT_APP_NOT_SECRET_CODE} />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/mock/cheat.ts
@@ -0,0 +1,101 @@
+import { CategoryProps } from "../components/cheat/types/CategoryProps";
+
+export const mockCategories: Array<CategoryProps> = [
+    {
+        title: "Customer Obsession",
+        stories: [
+            "Podcast Manager UI: Focus on the backend too",
+            "Fiori Excell Guy",
+        ]
+    },
+    {
+        title: "Ownership",
+        stories: [
+            "Distance site: Temporary Fulltime Dev",
+            "Make PestWeb not suck",
+            "IP Launchpad: I've never done this",
+        ]
+    },
+    {
+        title: "Invent and Simplify",
+        stories: [
+            "Fiori Launchpad App: Build Automation",
+            "Generated technical specifications from docblocks in SAPUI apps",
+            "PrePost model for SAPUI5",
+        ]
+    },
+    {
+        title: "Are Right, a Lot",
+        stories: [
+            "Waerhauser acquisition almost-mishap",
+            "Univar: SD Card Impending Doom",
+            "Document Database",
+        ]
+    },
+    {
+        title: "Learn and Be Curious",
+        stories: [
+            "Excell guy",
+            "CGBF: Led to my first real MVC experience",
+            "Casper",
+        ]
+    },
+    {
+        title: "Hire and Develop the Best",
+        stories: [
+            "Javascript workshop for Abapers",
+            "Hired my replacement at Distance"
+        ],
+    },
+    {
+        title: "Insist on the Highest Standards",
+        stories: [
+            "Make PestWeb not suck (even though it's dying)",
+            "Refusal for \"This is the way we always do it\" that I told Charley",
+            "Adhearence to Fiori UX Guidelines",
+        ]
+    },
+    {
+        title: "Think Big",
+        stories: [
+            "IP's Processes suck: Casper",
+            "Distance Redesign",
+            "LADI: Accidentally Attempting to rewrite a php framework"
+        ],
+    },
+    {
+        title: "Bias for Action",
+        stories: [
+            "Casper: We are doing scrum now",
+            "Make Pestweb UI Not Suck"
+        ],
+    },
+    {
+        title: "Frugality",
+        stories: [
+            "Using viral libraries for PrePost"
+        ],
+    },
+    {
+        title: "Earn Trust",
+        stories: [
+            "Univar: I am not being listened to",
+            "When things got tense with Jon, I gripped about the loud door"
+
+        ],
+    },
+    {
+        title: "Dive Deep",
+        stories: [
+            "IP Launchpad:  How to iOS/Hybrid/Airwatch"
+        ],
+    },
+    {
+        title: "Have Backbone; Disagree and Commit",
+        stories: ["Podcast MVC Conflict"],
+    },
+    {
+        title: "Deliver Result",
+        stories: ["Distance Redesign: Long Haul"],
+    },
+];
